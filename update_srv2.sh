039fa2750d3c6f07b83d98aad579a143de52ba0e
tmferreira-ti <163651394+tmferreira-ti@users.noreply.github.com>
2024-09-28 09:54:36 -0300

    Update update_srv2.sh

--- update_srv2.sh
+++ update_srv2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv2/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/refs/heads/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
 reboot
