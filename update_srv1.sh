1e3e828a3e1a3272d3a9ceb63427fd21152345d6
tmferreira-ti <163651394+tmferreira-ti@users.noreply.github.com>
2024-10-26 11:53:40 -0300

    .

--- update_srv1.sh
+++ update_srv1.sh
@@ -22,7 +22,7 @@ mkdir -p /etc/bind/domains/fatecseg/
 
 wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/db.fatecseg.edu.br -O /etc/bind/domains/fatecseg/db.fatecseg.edu.br --no-check-certificate --no-cache
 
-wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/db.0.17.172 -O /etc/bind/domains/fatecseg/db.fatecseg.edu.br --no-check-certificate --no-cache
+wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/db.0.17.172 -O /etc/bind/domains/fatecseg/db.0.17.172 --no-check-certificate --no-cache
 
 echo "domain fatecseg.edu.br" > /etc/resolv.conf
 echo "search fatecseg.edu.br"  >> /etc/resolv.conf
