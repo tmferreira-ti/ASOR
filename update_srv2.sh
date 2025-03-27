e226de97bbb521e05afa312aa9d76e276a94b596
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2025-03-27 10:45:45 -0300

    Update update_srv2.sh

--- update_srv2.sh
+++ update_srv2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv2/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
+wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv2/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
 #wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/refs/heads/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
 
 #apt install bind9 -y
