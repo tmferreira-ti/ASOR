b34d60a49f78664c90272676d8da3c182591a8dd
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2025-04-03 09:02:18 -0300

    Update update_srv1.sh

--- update_srv1.sh
+++ update_srv1.sh
@@ -4,7 +4,8 @@ wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv1/sysctl
 wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv1/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
 wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv1/nftables.conf -O /etc/nftables.conf --no-check-certificate --no-cache
 sed -i s'/enp0s8/enp0s3/g' /usr/local/bin/sync-update
-#wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/refs/heads/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
+apt install ssh -y
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/refs/heads/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
 
 #apt install isc-dhcp-server -y
 #wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/refs/heads/main/Confs/srv1/dhcpd.conf -O /etc/dhcp/dhcpd.conf --no-check-certificate --no-cache
