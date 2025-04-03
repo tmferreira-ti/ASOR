e16ca59055a2fef7f78e7f9d29ed180fac767e51
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2025-04-03 09:09:31 -0300

    Update update_srv2.sh

--- update_srv2.sh
+++ update_srv2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv2/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
+apt update
 apt install ssh -y
 wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/refs/heads/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
 
