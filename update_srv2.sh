c153be00a988ab492ded5c2b442079cde6a6964e
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2025-04-17 11:24:04 -0300

    Update update_srv2.sh

--- update_srv2.sh
+++ update_srv2.sh
@@ -4,11 +4,11 @@ apt update
 apt install ssh -y
 wget https://raw.githubusercontent.com/tmferreira-ti/SSORI/refs/heads/main/configs/sshd_config -O /etc/ssh/sshd_config --no-check-certificate --no-cache
 
-#apt install bind9 -y
+apt install bind9 -y
 
-#wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv2/dns/named.conf.options -O /etc/bind/named.conf.options
+wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv2/dns/named.conf.options -O /etc/bind/named.conf.options
 
-#wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv2/dns/named.conf.local -O /etc/bind/named.conf.local --no-check-certificate --no-cache
+wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv2/dns/named.conf.local -O /etc/bind/named.conf.local --no-check-certificate --no-cache
 
 #apt update
 
