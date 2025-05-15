6b4480dea5e4686d9bacc739846dcde96de70a4f
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2025-05-15 11:09:38 -0300

    Update update_srv1.sh

--- update_srv1.sh
+++ update_srv1.sh
@@ -15,24 +15,24 @@ wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/refs/heads/main/Confs/
 systemctl enable nftables
 #reboot
 
-#apt install bind9 -y
+apt install bind9 -y
 
-#wget github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/named.conf.options -O /etc/bind/named.conf.options
+wget github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/named.conf.options -O /etc/bind/named.conf.options
 
-#wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/named.conf.local -O /etc/bind/named.conf.local --no-check-certificate --no-cache
+wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/named.conf.local -O /etc/bind/named.conf.local --no-check-certificate --no-cache
 
-#mkdir -p /etc/bind/domains/fatecseg/
+mkdir -p /etc/bind/domains/fatecseg/
 
-#wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/db.fatecseg.edu.br -O /etc/bind/domains/fatecseg/db.fatecseg.edu.br --no-check-certificate --no-cache
+wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/db.fatecseg.edu.br -O /etc/bind/domains/fatecseg/db.fatecseg.edu.br --no-check-certificate --no-cache
 
-#wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/db.0.17.172 -O /etc/bind/domains/fatecseg/db.0.17.172 --no-check-certificate --no-cache
+wget https://github.com/tmferreira-ti/ASOR/raw/refs/heads/main/Confs/srv1/dns/db.0.17.172 -O /etc/bind/domains/fatecseg/db.0.17.172 --no-check-certificate --no-cache
 
-#echo "domain fatecseg.edu.br" > /etc/resolv.conf
-#echo "search fatecseg.edu.br"  >> /etc/resolv.conf
-#echo "nameserver 172.17.0.1" >> /etc/resolv.conf
-#echo "nameserver 172.17.0.2" >> /etc/resolv.conf
+echo "domain fatecseg.edu.br" > /etc/resolv.conf
+echo "search fatecseg.edu.br"  >> /etc/resolv.conf
+echo "nameserver 172.17.0.1" >> /etc/resolv.conf
+echo "nameserver 172.17.0.2" >> /etc/resolv.conf
 
-#chattr +i /etc/resolv.conf
+chattr +i /etc/resolv.conf
 
 #systemctl restart named
 reboot
